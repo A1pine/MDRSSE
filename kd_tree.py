5418f303a4abb9e7c3f0e4b325448266afa300ce
A1pine <me@xuning.eu.org>
2023-10-15 21:37:42 +0800

    A class for plain classical KD-tree

---
+++ kd_tree.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, point, left_child, right_child):
+        self.point = point
+        self.left_child = left_child
+        self.right_child = right_child
+
+def kd_tree(point_list, depth=0):
+    if len(point_list) <= 0:
+        return None
+
+    # Select axis based on depth so that axis cycles through all valid values
+    k = len(point_list[0])  # assumes all points have the same dimension
+    axis = depth % k
+
+    # Sort point list and choose median as pivot element
+    point_list.sort(key=lambda x: x[axis])
+    median = len(point_list) // 2  # choose median
+
+    # Create node and construct subtrees
+    return Node(
+        point_list[median],
+        kd_tree(point_list[:median], depth + 1),
+        kd_tree(point_list[median + 1:], depth + 1)
+    )
